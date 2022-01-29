d14f8a4938b6ac75b8d26622b9fa2ed218b6dc57
JordanYanakiev <jordan.yanakiev@gmail.com>
2022-01-29 12:06:04 +0200

    dded UI buttons on play scene

--- CleaNor/Assets/Scripts/UIManager.cs
+++ CleaNor/Assets/Scripts/UIManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public Text pointsText;
+    public Button pauseButton;
+    public Button mainMenuButton;
+    public bool isPaused = false;
     public int score;
 
     IEnumerator UpdatePoints()
@@ -17,11 +21,37 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    public void Pause()
+    {
+        if (!isPaused)
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+            pauseButton.image.color = Color.yellow;
+            pauseButton.GetComponentInChildren<Text>().text = "II";
+        }
+        else if (isPaused)
+        {
+            isPaused = false;
+            Time.timeScale = 1;
+            pauseButton.image.color = Color.green;
+            pauseButton.GetComponentInChildren<Text>().text = "Pause";
+        }
+    }
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        pauseButton = GameObject.Find("PauseButton").GetComponent<Button>();
+        mainMenuButton = GameObject.Find("MainMenuButton").GetComponent<Button>();
+        pauseButton.image.color = Color.green;
+        pauseButton.onClick.AddListener(Pause);
+        mainMenuButton.onClick.AddListener(MainMenu);
         pointsText.text = "Score: 0";
         StartCoroutine(UpdatePoints());
     }
